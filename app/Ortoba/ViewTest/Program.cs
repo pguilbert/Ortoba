40103dab0c6e13f1c9809d909e43960978e70b12
LASCAUX Martial <lascaux.martial@hotmail.fr>
2014-03-24 17:29:00 +0100

    add .dll and get team method

---
+++ app/Ortoba/ViewTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ortoba.Data;
+
+namespace ViewTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var teams = ApiManagement.Instance.GetTeams("http://127.0.0.1/BitBucket/ortoba/core.php/team/all");
+            
+
+            foreach (Team team in teams)
+            {
+                Console.WriteLine(team.Name);
+            }
+
+            Console.Read();
+        }
+    }
+}
